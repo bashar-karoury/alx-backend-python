3922a4b960fce8990c8aba558f13a91c80e5071d
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-13 16:26:15 +0300

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Basic annotations- add"""
+import typing
+
+
+def add(a: float, b: float) -> float:
+    """ add function that sum float numbers"""
+    c: float = a + b
+    return c
