6070b2f77d35d0ce080de478019c6227507e3a52
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-13 17:32:30 +0300

    8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,12 +3,12 @@
 from typing import Callable
 
 
-def make_multiplier(n: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ takes a float multiplier as argument and
         returns a function that multiplies a float by multiplier.
     """
     def multipli(multi: float) -> float:
         """ returned mulitplier """
-        return multi * n
+        return multi * multiplier
 
     return multipli
