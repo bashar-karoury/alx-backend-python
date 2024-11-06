05d1929520a82e126759a9b01928fd09fbe925d2
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-06 23:56:39 +0300

    Update: test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,10 +25,26 @@ class TestGithubOrgClient(TestCase):
         mock_get_json.assert_called_once_with(called_with)
 
     def test_public_repos_url(self):
-        """ test if org returns correct result"""
+        """ test if _public_repos_url returns correct result"""
         client = GithubOrgClient('url')
         with patch(
             'client.GithubOrgClient.org',
                 new_callable=PropertyMock) as mock_got:
             mock_got.return_value = {'repos_url': 'expected_url'}
             self.assertEqual(client._public_repos_url, 'expected_url')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ test if public_repos returns correct result"""
+        client = GithubOrgClient('url')
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock) as mock_public_repo_url:
+            # set return value of _public_repos_url property
+            returned_repor_url = 'google.com'
+            mock_public_repo_url.return_value = returned_repor_url
+            mock_get_json.return_value = [{'name': 'googleDocs'}, {
+                'name': 'googleSlides'}, {'name': 'googleSheets'}]
+            expected_result = ['googleDocs', 'googleSlides', 'googleSheets']
+            self.assertEqual(client.public_repos(), expected_result)
+            mock_get_json.assert_called_once_with(returned_repor_url)
