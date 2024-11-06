3fd797f798f6c68d810ea77bb1c36b9909c25e5e
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-07 00:02:02 +0300

    Update: test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,27 +11,27 @@ from client import GithubOrgClient
 
 class TestGithubOrgClient(TestCase):
     """ Test Class for org method"""
-    @parameterized.expand([
-        ('google', 'google'),
-        ('abc', 'abc')
-    ])
-    @patch('client.get_json')
-    def test_org(self, url, expected, mock_get_json):
-        """ test if org returns correct result"""
-        client = GithubOrgClient(url)
-        mock_get_json.return_value = expected
-        self.assertEqual(client.org, expected)
-        called_with = client.ORG_URL.format(org=client._org_name)
-        mock_get_json.assert_called_once_with(called_with)
+    # @parameterized.expand([
+    #     ('google', 'google'),
+    #     ('abc', 'abc')
+    # ])
+    # @patch('client.get_json')
+    # def test_org(self, url, expected, mock_get_json):
+    #     """ test if org returns correct result"""
+    #     client = GithubOrgClient(url)
+    #     mock_get_json.return_value = expected
+    #     self.assertEqual(client.org, expected)
+    #     called_with = client.ORG_URL.format(org=client._org_name)
+    #     mock_get_json.assert_called_once_with(called_with)
 
-    def test_public_repos_url(self):
-        """ test if _public_repos_url returns correct result"""
-        client = GithubOrgClient('url')
-        with patch(
-            'client.GithubOrgClient.org',
-                new_callable=PropertyMock) as mock_got:
-            mock_got.return_value = {'repos_url': 'expected_url'}
-            self.assertEqual(client._public_repos_url, 'expected_url')
+    # def test_public_repos_url(self):
+    #     """ test if _public_repos_url returns correct result"""
+    #     client = GithubOrgClient('url')
+    #     with patch(
+    #         'client.GithubOrgClient.org',
+    #             new_callable=PropertyMock) as mock_got:
+    #         mock_got.return_value = {'repos_url': 'expected_url'}
+    #         self.assertEqual(client._public_repos_url, 'expected_url')
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
